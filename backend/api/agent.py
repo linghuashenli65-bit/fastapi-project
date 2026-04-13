1679211f46ea4552516b4290670d73c6f4d4e962
魏辉123 <2764411488@qq.com>
2026-04-13 16:13:50 +0800

    格式化ai分析输出

---
+++ backend/api/agent.py
@@ -0,0 +1,80 @@
+import json
+from typing import Optional
+
+import uvicorn
+from fastapi import FastAPI, APIRouter, HTTPException, Depends
+import sys,os
+
+from pydantic import BaseModel
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from backend.core.database import get_async_db
+from backend.crud.sql_service import execute_sql
+from backend.model.agent import agent_sql, dispatch_agent, generate_sql, generate_analysis
+from backend.model.dashboard import build_option, build_dashboard
+from backend.schemas.agent import SQLRequest, QueryRequest
+
+router = APIRouter()
+# 允许跨域
+
+
+# -------------------------------
+# 1. Agent SQL 普通查询接口
+# -------------------------------
+@router.post("/sql",summary="ai查询接口")
+def agent_sql_api(req: QueryRequest, model:str= ""):
+    try:
+        return agent_sql(req.query, model)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+# -------------------------------
+# 2. Agent调度
+# -------------------------------
+@router.post("/dispatch",summary="ai调度接口（测试）")
+def dispatch_api(req: QueryRequest):
+    return dispatch_agent(req.query)
+
+
+# -------------------------------
+# 3. SQL生成
+# -------------------------------
+@router.post("/generate",summary="sql生成接口（测试）")
+def generate_sql_api(req: QueryRequest):
+
+    sql = generate_sql(req.query)
+    return {"code": 200, "sql": sql}
+
+
+# -------------------------------
+# 4. SQL执行
+# -------------------------------
+@router.post("/execute",summary="sql执行接口（测试）")
+def execute_sql_api(req: SQLRequest):
+    try:
+        data = execute_sql(req.sql)
+        return {"code": 200, "data": data}
+    except Exception as e:
+        return {"code": 500, "msg": str(e)}
+from fastapi.responses import StreamingResponse
+
+
+class DashboardRequest(BaseModel):
+    query: str
+    model: str="qwen"
+    analysis_length: Optional[str] = "medium"
+# -------------------------------
+# 5. ai分析接口
+# -------------------------------
+@router.post("/dashboard", summary="AI分析流式接口")
+async def dashboard_stream(req: DashboardRequest):
+    async def event_generator():
+        async for event in build_dashboard(req.query, req.model, req.analysis_length):
+            # 将字典转换为 SSE 格式的字符串
+            yield f"data: {json.dumps(event, ensure_ascii=False)}\n\n"
+    return StreamingResponse(event_generator(), media_type="text/event-stream")
+
+
+if __name__ == '__main__':
+    uvicorn.run(router, host="0.0.0.0", port=8888)
+
+
